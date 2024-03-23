160c269489b279642457776f2bc6df4216615f23
Khuu Van Nguyen <khuuvannguyen@gmail.com>
2024-03-23 21:41:32 +0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function App(): React.JSX.Element {
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>asdasdasd</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
+
+export default App;
